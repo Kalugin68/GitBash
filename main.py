e129f755075af2148cf41e1cad121ced91ffbeea
Kalugin68 <sashakalugin74@gmail.com>
2025-03-09 18:46:28 +0300

    Add sort function

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ def print_hi(name):
     print(f'Hi, {name}')
 
 
+def sort_list(lst):
+    lst.sort()
+    return lst
+
+
 def main():
     lastname = input("Enter Your lastname: ")
     lastname = lastname.lower()
     print_hi(lastname)
+
+    lst = [123, 1.2, 34, 67, 1, 98, 134, 23, 54]
+    print(sort_list(lst))
 
 
 main()
